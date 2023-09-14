01f6824349716c20bbb813161ce248d3fa55127f
LGROUP\Takeshi <Takeshi@xyzzdev.com>
2023-09-14 15:10:21 +0800

    *

---
+++ rabbitmq/listener.go
@@ -0,0 +1,90 @@
+package rabbitmq
+
+import (
+	"log"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+	"github.com/zeromicro/go-zero/core/logx"
+	"github.com/zeromicro/go-zero/core/queue"
+)
+
+type (
+	// ConsumeHandle func(message string) error
+
+	ConsumeHandler interface {
+		Consume(message string) error
+	}
+
+	RabbitListener struct {
+		conn    *amqp.Connection
+		channel *amqp.Channel
+		forever chan bool
+		handler map[string]ConsumeHandler
+		queues  RabbitListenerConf
+	}
+)
+
+func MustNewListener(listenerConf RabbitListenerConf, handler map[string]ConsumeHandler) queue.MessageQueue {
+	listener := &RabbitListener{queues: listenerConf, handler: handler, forever: make(chan bool)}
+	conn, err := amqp.Dial(getRabbitURL(listenerConf.RabbitConf))
+	if err != nil {
+		log.Fatalf("failed to connect rabbitmq, error: %v", err)
+	}
+
+	listener.conn = conn
+	channel, err := listener.conn.Channel()
+	if err != nil {
+		log.Fatalf("failed to open a channel: %v", err)
+	}
+
+	listener.channel = channel
+	return listener
+}
+
+func (q RabbitListener) Start() {
+	for _, que := range q.queues.ListenerQueues {
+		err := q.consume(que)
+		if err != nil {
+			logx.Error(err)
+		}
+	}
+
+	<-q.forever
+}
+
+func (q RabbitListener) consume(que ConsumerConf) error {
+	msg, err := q.channel.Consume(
+		que.Name,
+		"",
+		que.AutoAck,
+		que.Exclusive,
+		que.NoLocal,
+		que.NoWait,
+		nil,
+	)
+	if err != nil {
+		log.Fatalf("failed to listener, error: %v", err)
+		return err
+	}
+
+	go func() {
+		for d := range msg {
+			logx.Debug("接受到數據", d.Body)
+			if handle, ok := q.handler[que.Name]; ok == true {
+				if err := handle.Consume(string(d.Body)); err != nil {
+					logx.Error("Error on consuming: %s, error: %v", string(d.Body), err)
+				}
+			} else {
+				logx.Error("消费者不存在，请检查配置")
+			}
+		}
+	}()
+
+	return nil
+}
+
+func (q RabbitListener) Stop() {
+	q.channel.Close()
+	q.conn.Close()
+	close(q.forever)
+}
